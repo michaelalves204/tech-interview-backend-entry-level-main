cd84b964cfb95d67520983bf5d3daa7e2c1adb79
michaelalves204 <michaelalvesreino404@outlook.com>
2026-04-17 19:28:02 -0300

    feat(cart): add cart serializer for API response

---
+++ app/serializers/cart_serializer.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+class CartSerializer
+  def initialize(cart)
+    @cart = cart
+  end
+
+  def as_json(*)
+    {
+      id: @cart.id,
+      products: products,
+      total_price: @cart.total_price.to_f
+    }
+  end
+
+  private
+
+  def cart_items
+    @cart_items ||= @cart.cart_items.includes(:product)
+  end
+
+  def products
+    cart_items.map do |item|
+      {
+        id: item.product.id,
+        name: item.product.name,
+        quantity: item.quantity,
+        unit_price: item.unit_price,
+        total_price: item.quantity * item.unit_price
+      }
+    end
+  end
+end
